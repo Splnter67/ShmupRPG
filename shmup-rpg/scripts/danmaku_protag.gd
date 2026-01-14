fd9dbc745f0a05705926ff24fc7044a5401da3a4
Splnter67 <77417062+Splnter67@users.noreply.github.com>
2026-01-13 21:54:41 -0500

    changed radius?

--- shmup-rpg/scripts/danmaku_protag.gd
+++ shmup-rpg/scripts/danmaku_protag.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
-@export var Speed = 20000
-@export var FocusSpeed = 13500
+@export var Speed = 300
+@export var FocusSpeed = 200
 var CurrentSpeed : float
 
 # Called when the node enters the scene tree for the first time.
@@ -16,6 +16,6 @@ func _physics_process(delta: float):
 		CurrentSpeed = Speed
 	var HorizontalInput = Input.get_action_strength("right") - Input.get_action_strength("left")
 	var VerticalInput = Input.get_action_strength("down") - Input.get_action_strength("up")
-	velocity.x = HorizontalInput * CurrentSpeed * delta
-	velocity.y = VerticalInput * CurrentSpeed * delta
+	velocity.x = HorizontalInput * CurrentSpeed * delta * 60
+	velocity.y = VerticalInput * CurrentSpeed * delta * 60
 	move_and_slide()
